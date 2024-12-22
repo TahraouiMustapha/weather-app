053ea62597f47f85802fafcc18b9a940add51b81
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2024-12-22 20:17:35 +0100

    feat: install date-fns package to manipulate dates, style the cards container

--- src/script.js
+++ src/script.js
@@ -1,16 +1,23 @@
+import { format } from "date-fns";
+import "./style.css";
 // const myKey = "SBB3V7KN7UXJVYQBVJ369S95Z";
-
 // https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=YOUR_API_KEY 
 
 const locationInput = document.querySelector('#locationInput');
 const fetchBtn = document.querySelector('#fetchBtn');
 
-fetchBtn.addEventListener('click', async () => {
+
+fetchBtn.addEventListener('input', async (e) => {
+    e.preventDefault();
     const location = locationInput.value;
     try {
         if(location) {
             const myObj = await getWeatherData(location);
-            console.log(myObj);
+            for(let key in myObj) {
+                console.log(myObj[key]);
+            }
+            console.log(myObj.address, myObj.resolvedAddress);
+            console.log(myObj.conditions);
         } else {
             throw new Error("you should fill right location!");
         }
@@ -21,18 +28,20 @@ fetchBtn.addEventListener('click', async () => {
 })
 
 async function getWeatherData(lacation) {
+    const today = format(new Date(), 'yyyy-MM-dd');
     try {    
-        const myPromise = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lacation}/2024-12-22?key=SBB3V7KN7UXJVYQBVJ369S95Z`, {
+        const myPromise = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lacation}/${today}?key=SBB3V7KN7UXJVYQBVJ369S95Z`, {
             mode: "cors",
         });
 
         const myObject = await myPromise.json();
-        return myObject.days[0];
+        return myObject;
     } catch(err) {
-        console.log('the error in getWeatherData func');
+        console.log('can\'t get this information');
         throw err;
     }
 }
+
 
 
 
