ff15cf41cb136e72de2ce10373c8d7cb67a2d6e5
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2024-12-22 11:43:41 +0100

    feat : create a  func to take the location and get the weather data of that location

--- src/script.js
+++ src/script.js
@@ -1,1 +1,21 @@
-console.log("ji");
+// const myKey = "SBB3V7KN7UXJVYQBVJ369S95Z";
+
+// https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=YOUR_API_KEY 
+
+
+async function getWeatherData(lacation) {
+    try {    
+        const myPromise = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lacation}/2024-12-22?key=SBB3V7KN7UXJVYQBVJ369S95Z`, {
+            mode: "cors",
+        });
+
+        const myObject = await myPromise.json();
+        console.log(myObject);
+    } catch(err) {
+        console.log(err)
+    }
+
+}
+
+
+getWeatherData('bouira');
