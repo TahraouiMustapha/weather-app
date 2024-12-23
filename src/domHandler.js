464daebb264c97a758d232ab3d9e1768b0fef3b2
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2024-12-23 20:45:13 +0100

    pretty code : test prettier , eslint

--- src/domHandler.js
+++ src/domHandler.js
@@ -5,173 +5,179 @@ import sunIcon from "./assets/sun.png";
 import windIcon from "./assets/wind.png";
 
 function domHandler(obj) {
-    const container = document.querySelector('.cards-container');
-    const smallCardsContainer = document.querySelector('.small-cards-container');
-    resetDom();
-    container.insertBefore(createBigCard(obj), smallCardsContainer);
-    
-    const days = obj.days;
-    for(let i = 1; i < 10; i++) {
-        smallCardsContainer.appendChild(createSmallCard(days[i]));
-    }
+  const container = document.querySelector(".cards-container");
+  const smallCardsContainer = document.querySelector(".small-cards-container");
+  resetDom();
+  container.insertBefore(createBigCard(obj), smallCardsContainer);
 
-} 
+  const days = obj.days;
+  for (let i = 1; i < 10; i++) {
+    smallCardsContainer.appendChild(createSmallCard(days[i]));
+  }
+}
 
 function createBigCard(obj) {
-    const day0 = obj.days[0];
-    const myDiv = document.createElement('div');
-    myDiv.classList.add('big-card');
-    
-    myDiv.appendChild(cardBuilder.createIcon(pickIcon(day0.icon)));
+  const day0 = obj.days[0];
+  const myDiv = document.createElement("div");
+  myDiv.classList.add("big-card");
 
-    const informationDiv = document.createElement('div');
-    informationDiv.classList.add('information');
-    informationDiv.appendChild(cardBuilder.createTemp(day0.temp));
-    informationDiv.appendChild(cardBuilder.createAddress(obj.resolvedAddress));
-    informationDiv.appendChild(cardBuilder.createDescription(obj.description));
+  myDiv.appendChild(cardBuilder.createIcon(pickIcon(day0.icon)));
 
-    myDiv.appendChild(informationDiv);
+  const informationDiv = document.createElement("div");
+  informationDiv.classList.add("information");
+  informationDiv.appendChild(cardBuilder.createTemp(day0.temp));
+  informationDiv.appendChild(cardBuilder.createAddress(obj.resolvedAddress));
+  informationDiv.appendChild(cardBuilder.createDescription(obj.description));
 
-    return myDiv;
+  myDiv.appendChild(informationDiv);
+
+  return myDiv;
 }
- 
+
 function createSmallCard(dayObj) {
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const smallCard = document.createElement('div');
-    smallCard.classList.add('small-card');
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const smallCard = document.createElement("div");
+  smallCard.classList.add("small-card");
 
-    smallCard.appendChild(cardBuilder.createIcon(pickIcon(dayObj.icon)));
+  smallCard.appendChild(cardBuilder.createIcon(pickIcon(dayObj.icon)));
 
-    const informationDiv = document.createElement('div');
-    informationDiv.classList.add('information');
-    informationDiv.appendChild(cardBuilder.createTemp(dayObj.temp));
-    const dayName = days[new Date(dayObj.datetime).getDay()];
-    informationDiv.appendChild(cardBuilder.createDayDiv(dayName));
-    informationDiv.appendChild(cardBuilder.createDateDiv(dayObj.datetime));
-    informationDiv.appendChild(cardBuilder.createDescription(dayObj.conditions));
+  const informationDiv = document.createElement("div");
+  informationDiv.classList.add("information");
+  informationDiv.appendChild(cardBuilder.createTemp(dayObj.temp));
+  const dayName = days[new Date(dayObj.datetime).getDay()];
+  informationDiv.appendChild(cardBuilder.createDayDiv(dayName));
+  informationDiv.appendChild(cardBuilder.createDateDiv(dayObj.datetime));
+  informationDiv.appendChild(cardBuilder.createDescription(dayObj.conditions));
 
-    smallCard.appendChild(informationDiv);
+  smallCard.appendChild(informationDiv);
 
-    return smallCard;
+  return smallCard;
 }
 
 function resetDom() {
-    const bigCard = document.querySelector('.big-card');
-    if(bigCard) bigCard.remove();
-    const smallCardsContainer = document.querySelector('.small-cards-container');
-    smallCardsContainer.innerHTML = '';
+  const bigCard = document.querySelector(".big-card");
+  if (bigCard) bigCard.remove();
+  const smallCardsContainer = document.querySelector(".small-cards-container");
+  smallCardsContainer.innerHTML = "";
 }
 
 function pickIcon(objDotIcon) {
-    const myIcons = [
-        {   
-            iconName: cloudyIcon,
-            str:"cloudy"
-        }, 
-        {   
-            iconName: rainIcon,
-            str:"rain"
-        },
-        {   
-            iconName: snowIcon,
-            str:"snow"
-        },
-        {   
-            iconName: sunIcon,
-            str:"sun"
-        },
-        {
-            iconName: windIcon,
-            str:"wind"
-        }
-    ];
-    const rightIcon = myIcons.find((icon) => objDotIcon.includes(icon.str)); 
-    if(!rightIcon) return sunIcon;
-    return rightIcon.iconName; 
+  const myIcons = [
+    {
+      iconName: cloudyIcon,
+      str: "cloudy",
+    },
+    {
+      iconName: rainIcon,
+      str: "rain",
+    },
+    {
+      iconName: snowIcon,
+      str: "snow",
+    },
+    {
+      iconName: sunIcon,
+      str: "sun",
+    },
+    {
+      iconName: windIcon,
+      str: "wind",
+    },
+  ];
+  const rightIcon = myIcons.find((icon) => objDotIcon.includes(icon.str));
+  if (!rightIcon) return sunIcon;
+  return rightIcon.iconName;
 }
 
-const handleLoadingMessage = (function() {
-    const loading = document.querySelector('.loading');
-    const container = document.querySelector('.cards-container');
+const handleLoadingMessage = (function () {
+  const loading = document.querySelector(".loading");
+  const container = document.querySelector(".cards-container");
 
-    const showLoading = () => {
-        loading.style.display = 'block';
-        container.style.display = 'none';
-    }
+  const showLoading = () => {
+    loading.style.display = "block";
+    container.style.display = "none";
+  };
 
-    const hideLoading = () => {
-        loading.style.display = 'none';
-        container.style.display = 'flex';
-    }
+  const hideLoading = () => {
+    loading.style.display = "none";
+    container.style.display = "flex";
+  };
 
-    return {
-        showLoading,
-        hideLoading
-    }
+  return {
+    showLoading,
+    hideLoading,
+  };
 })();
 
-const cardBuilder = (function (){
-    const createIcon = (weatherIcon) => {
-        const myDiv = document.createElement('div');
-        myDiv.classList.add('icon');
-            const img = document.createElement('img');
-            img.src = weatherIcon;
-            img.alt = "icon";
+const cardBuilder = (function () {
+  const createIcon = (weatherIcon) => {
+    const myDiv = document.createElement("div");
+    myDiv.classList.add("icon");
+    const img = document.createElement("img");
+    img.src = weatherIcon;
+    img.alt = "icon";
 
-        myDiv.appendChild(img);    
-        return myDiv;
-    }
+    myDiv.appendChild(img);
+    return myDiv;
+  };
 
-    const createTemp = (temp) => {
-        const myDiv = document.createElement('div');
-        myDiv.classList.add('temp');
-        myDiv.textContent = temp;
+  const createTemp = (temp) => {
+    const myDiv = document.createElement("div");
+    myDiv.classList.add("temp");
+    myDiv.textContent = temp;
 
-        return myDiv;
-    }
+    return myDiv;
+  };
 
-    const createAddress = (address) => {
-        const myDiv = document.createElement('div');
-        myDiv.classList.add('address');
-        myDiv.textContent = address;
+  const createAddress = (address) => {
+    const myDiv = document.createElement("div");
+    myDiv.classList.add("address");
+    myDiv.textContent = address;
 
-        return myDiv;
-    }
+    return myDiv;
+  };
 
-    const createDescription = (desc) => {
-        const myDiv = document.createElement('div');
-        myDiv.classList.add('description');
-        myDiv.textContent = desc;
+  const createDescription = (desc) => {
+    const myDiv = document.createElement("div");
+    myDiv.classList.add("description");
+    myDiv.textContent = desc;
 
-        return myDiv;
-    }
+    return myDiv;
+  };
 
-    const createDayDiv = (day) => {
-        const myDiv = document.createElement('div');
-        myDiv.classList.add('day');
-        myDiv.textContent = day;
+  const createDayDiv = (day) => {
+    const myDiv = document.createElement("div");
+    myDiv.classList.add("day");
+    myDiv.textContent = day;
 
-        return myDiv;
-    }
+    return myDiv;
+  };
 
-    const createDateDiv = (date = '') => {
-        const myDiv = document.createElement('div');
-        myDiv.classList.add('date');
-        myDiv.textContent = `(${date})`;
+  const createDateDiv = (date = "") => {
+    const myDiv = document.createElement("div");
+    myDiv.classList.add("date");
+    myDiv.textContent = `(${date})`;
 
-        return myDiv;
-    }
+    return myDiv;
+  };
 
-    return {
-        createIcon,
-        createTemp,
-        createAddress,
-        createDescription,
-        createDayDiv,
-        createDateDiv,
-    }
+  return {
+    createIcon,
+    createTemp,
+    createAddress,
+    createDescription,
+    createDayDiv,
+    createDateDiv,
+  };
 })();
-
 
 export default domHandler;
 export { handleLoadingMessage };
--- src/script.js
+++ src/script.js
@@ -2,50 +2,44 @@ import { format, addDays } from "date-fns";
 import "./style.css";
 import sendObjToDom, { handleLoadingMessage } from "./domHandler.js";
 
-
-const locationInput = document.querySelector('#locationInput');
-const fetchBtn = document.querySelector('#fetchBtn');
+const locationInput = document.querySelector("#locationInput");
+const fetchBtn = document.querySelector("#fetchBtn");
 // for begin
 handleLoadingMessage.hideLoading();
 
-fetchBtn.addEventListener('click', async (event) => {
-    event.preventDefault();
-    const location = locationInput.value;
-    handleLoadingMessage.showLoading();
-    try {
-        if(location) {
-            const myObj = await getWeatherData(location);
-            handleLoadingMessage.hideLoading();
-            sendObjToDom(myObj);
-        } else {
-            throw new Error("you should fill right location!");
-        }
-    } catch(err) {
-        handleLoadingMessage.hideLoading();
-        alert(err);
+fetchBtn.addEventListener("click", async (event) => {
+  event.preventDefault();
+  const location = locationInput.value;
+  handleLoadingMessage.showLoading();
+  try {
+    if (location) {
+      const myObj = await getWeatherData(location);
+      handleLoadingMessage.hideLoading();
+      sendObjToDom(myObj);
+    } else {
+      throw new Error("you should fill right location!");
     }
-
-})
- 
+  } catch (err) {
+    handleLoadingMessage.hideLoading();
+    alert(err);
+  }
+});
 
 async function getWeatherData(lacation) {
-    const today = format(new Date(), 'yyyy-MM-dd');
-    const date2 = format(addDays(today, 10), 'yyyy-MM-dd');
-    try {    
-        const myPromise = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lacation}/${today}/${date2}?key=SBB3V7KN7UXJVYQBVJ369S95Z`, {
-            mode: "cors",
-        });
+  const today = format(new Date(), "yyyy-MM-dd");
+  const date2 = format(addDays(today, 10), "yyyy-MM-dd");
+  try {
+    const myPromise = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lacation}/${today}/${date2}?key=SBB3V7KN7UXJVYQBVJ369S95Z`,
+      {
+        mode: "cors",
+      },
+    );
 
-        const myObject = await myPromise.json();
-        return myObject;
-    } catch(err) {
-        alert('can\'t get this information');
-        throw err;
-    }
+    const myObject = await myPromise.json();
+    return myObject;
+  } catch (err) {
+    alert("can't get this information");
+    throw err;
+  }
 }
-
-
-
-
-
-
